6b2147b46f44eec1b12598482ade91e922b266e0
Meet Raut <meetraut3004@gmail.com>
2024-05-22 14:41:39 +0530

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clerk-Auth-App
 Next.js &amp; React Authentication With Clerk
+
+```bash 
+npm run dev
+```
--- clerk-app/app/page.jsx
+++ clerk-app/app/page.jsx
@@ -2,7 +2,7 @@ export default function Home(){
   return(
     <>
       <h1 className="text-2xl font-bold mb-5">Home</h1>
-      <p className="mb-5">This is demo site for clerk</p>
+      <p className="mb-5">This is home page</p>
     </>
   )
 }
